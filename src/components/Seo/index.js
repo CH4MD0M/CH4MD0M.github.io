8db8ede92ad884fcd856f8d731e7df6c124db129
KiHoon Roh <started677@gmail.com>
2022-11-18 23:02:56 +0900

    [refact]Seo 최적화

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -2,52 +2,26 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-const Seo = ({ description, title }) => {
+const Seo = ({ title, description, url }) => {
   const { site } = useStaticQuery(seoQuery);
   const metaDescription = description || site.siteMetadata.description;
+  const metaUrl = url || site.siteMetadata.siteUrl;
 
   return (
-    <Helmet
-      htmlAttributes={{ lang: "ko" }}
-      title={title}
-      defaultTitle={site.siteMetadata.title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:site_title`,
-          content: title,
-        },
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: "og:author",
-          content: site.siteMetadata.author,
-        },
-        {
-          property: "og:image",
-          content: `${site.siteMetadata.siteUrl}/og-image.png`,
-        },
-
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: "google-site-verification",
-          content: "Td2FVAr2l6Zwnul0s2sHek3QuYryO1uejM59c8zFcNk",
-        },
-      ]}
-    />
+    <Helmet>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content={metaUrl} />
+      <meta property="og:description" content={metaDescription} />
+      <meta
+        property="og:image"
+        content={`${site.siteMetadata.siteUrl}/og-image.png`}
+      />
+      <meta
+        name="google-site-verification"
+        content="Td2FVAr2l6Zwnul0s2sHek3QuYryO1uejM59c8zFcNk"
+      />
+    </Helmet>
   );
 };
 
@@ -57,10 +31,7 @@ const seoQuery = graphql`
   query {
     site {
       siteMetadata {
-        title
         siteUrl
-        description
-        author
       }
     }
   }
