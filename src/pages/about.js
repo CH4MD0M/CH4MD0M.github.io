1f6e74f773fa24a3e008c2e9116f028203b32c53
KiHoon Roh <started677@gmail.com>
2022-11-25 23:18:59 +0900

    [ui]about 페이지 수정

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,16 @@ import Layout from "../layout";
 import Seo from "../components/Seo";
 import PageTitle from "../components/PageTitle";
 import Bio from "../components/Bio";
+import Divider from "../components/Divider";
 
 const AboutPage = () => {
   return (
     <Layout>
       <Seo title="About" />
       <PageTitle>About.</PageTitle>
+      <Divider mt="4rem" />
       <Bio />
+      <Divider />
     </Layout>
   );
 };
