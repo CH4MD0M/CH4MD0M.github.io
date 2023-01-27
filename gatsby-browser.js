8d2ca71781c7d87e8a471999088364e2f2d62038
KiHoon Roh <started677@gmail.com>
2023-01-27 21:32:44 +0900

    [feat]페이지 이동시 스크롤 위치 유지 기능 구현

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./src/state/store";
+import { store } from "./src/store";
 
 export const wrapRootElement = ({ element }) => (
   <Provider store={store}>{element}</Provider>
 );
+
+const transitionDelay = 100;
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  window.history.scrollRestoration = "manual";
+  const currentPosition = getSavedScrollPosition(location);
+  window.setTimeout(() => {
+    window.scrollTo(...currentPosition);
+  }, transitionDelay);
+  return false;
+};
