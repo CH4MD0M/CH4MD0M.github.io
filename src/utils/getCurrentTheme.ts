869f20087a11b14f352769be7a8465d07effaede
KiHoon Roh <started677@gmail.com>
2023-06-24 23:43:27 +0900

    [fix]타입 정의 오류 수정

--- src/utils/getCurrentTheme.ts
+++ src/utils/getCurrentTheme.ts
@@ -1,12 +1,12 @@
 import { getValue } from './storage';
 
-export function getCurrentTheme() {
+export function getCurrentTheme(): ThemeMode {
   const localTheme = getValue('themeMode');
-  if (localTheme) return localTheme as ThemeMode;
+  if (localTheme) return localTheme;
 
   if (typeof window !== 'undefined') {
     const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
     const systemTheme = colorSchemeQuery.matches ? 'dark' : 'light';
-    return systemTheme as ThemeMode;
+    return systemTheme;
   }
 }
