1a2d586501ce7075fc516e321da65e128a3bc85d
KiHoon Roh <started677@gmail.com>
2024-07-29 01:55:16 +0900

    [update]PostList 수정
    - 타입 재정의
    - postPreview date margin 추가

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import useInfiniteScroll from '@hooks/useInfiniteScroll';
+import Divider from '@components/Divider';
 import PostPreview from '@components/PostPreview';
 
-interface PostListProps {
-  postList: Queries.IndexPageQuery['allMdx']['nodes'];
-}
+type PostListProps = {
+  postList:
+    | Queries.TagsPageQuery['allMdx']['nodes']
+    | Queries.CategoryPageQuery['allMdx']['nodes']
+    | Queries.SearchPageQuery['allMdx']['nodes'];
+};
 
 const PostList = ({ postList }: PostListProps) => {
   const [count, setCount] = useState(10);
@@ -24,6 +28,7 @@ const PostList = ({ postList }: PostListProps) => {
 
   return (
     <>
+      {/* {postList.length ? <Divider mt="6rem" mb="3rem" /> : <></>} */}
       {postList.slice(0, count).map(post => {
         return <PostPreview key={post.id} post={post} />;
       })}
